a972c892997fd6e113bbe9c55fc273d815797b87
anton2920 <anton2920@gmail.com>
2024-07-22 17:52:49 +0300

    Added fortunes benchmark for fasthttp/templ

--- fasthttp/main.go
+++ fasthttp/main.go
@@ -1,20 +1,61 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"sort"
 
 	"github.com/valyala/fasthttp"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var HelloWorld = []byte("Hello, world!\n")
 
 func PlaintextHandler(ctx *fasthttp.RequestCtx) {
-	if string(ctx.Path()) == "/plaintext" {
+	switch string(ctx.Path()) {
+	case "/plaintext":
 		ctx.Write(HelloWorld)
+	case "/fortunes":
+		FortunesHandler(ctx)
 	}
 }
 
+type Fortune struct {
+	ID      int
+	Message string
+}
+
+var DB *pgxpool.Pool
+
+func FortunesHandler(ctx *fasthttp.RequestCtx) {
+	rows, _ := DB.Query(context.Background(), "SELECT id, message FROM fortunes")
+	fortunes, err := pgx.CollectRows(rows, pgx.RowToStructByPos[Fortune])
+	if err != nil {
+		ctx.SetStatusCode(http.StatusInternalServerError)
+		fmt.Fprintf(ctx, "Failed to read fortunes from DB: %v", err)
+		return
+	}
+	fortunes = append(fortunes, Fortune{ID: len(fortunes), Message: "Additional fortune added at request time."})
+
+	sort.Slice(fortunes, func(i, j int) bool {
+		return fortunes[i].Message < fortunes[j].Message
+	})
+
+	component := FortunesTempl(fortunes)
+	component.Render(context.Background(), ctx)
+}
+
 func main() {
+	var err error
+	DB, err = pgxpool.New(context.Background(), "postgres://postgres:pass@localhost:5432/techempower")
+	if err != nil {
+		log.Fatalf("Failed to connect to a database: %v", err)
+	}
+
 	log.Printf("Listening on 0.0.0.0:7073...")
 	log.Fatal(fasthttp.ListenAndServe("0.0.0.0:7073", PlaintextHandler))
 }
--- gin/go.mod
+++ gin/go.mod
@@ -1,6 +1,6 @@
 module github.com/anton2920/techempower/gin
 
-go 1.22.4
+go 1.22.2
 
 require github.com/gin-gonic/gin v1.10.0
 
